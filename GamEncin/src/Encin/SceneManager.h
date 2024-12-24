ebdc23c64543440fdf3477ec855c22ee0f6954f1
omerfuyar <denemeyo@gmail.com>
2024-12-24 14:53:00 +0200

    fucked up all folders and solutions and projects and links

--- GamEncin/src/Encin/SceneManager.h
+++ GamEncin/src/Encin/SceneManager.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Components.h"
 #include <chrono>
+#include <GLFW/glfw3.h>
 #include <thread>
 #include <vector>
 using namespace std::chrono;
@@ -118,7 +119,36 @@ namespace GamEncin
 
 		static void Render()
 		{
-			
+			GLFWwindow* window = nullptr;
+
+			/* Initialize the library */
+			if(!glfwInit())
+
+				/* Create a windowed mode window and its OpenGL context */
+				window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+			if(!window)
+			{
+				glfwTerminate();
+			}
+
+			/* Make the window's context current */
+			glfwMakeContextCurrent(window);
+
+			/* Loop until the user closes the window */
+			while(!glfwWindowShouldClose(window))
+			{
+				/* Render here */
+				glClear(GL_COLOR_BUFFER_BIT);
+
+				/* Swap front and back buffers */
+				glfwSwapBuffers(window);
+
+				/* Poll for and process events */
+				glfwPollEvents();
+			}
+
+			glfwTerminate();
 		}
+
 	};
 }
