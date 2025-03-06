853d9397457cf66880a16d692e6c39a8bc0777a0
omerfuyar <denemeyo@gmail.com>
2025-03-06 12:33:06 +0200

    fucked up includes

--- GamEncin/src/User/main.cpp
+++ GamEncin/src/User/main.cpp
@@ -17,19 +17,20 @@ void SceneBuilding(Application& app)
         Vector3(0.5, 0.5, 0.5), Vector3(0, 255, 255),
         Vector3(0.5, 0.5, -0.5), Vector3(255, 255, 255),
         Vector3(-0.5, 0.5, -0.5), Vector3(0, 0, 0)
+
     };
-
-    object1.scale = Vector3(0.5, 1.6, 1);
-
-    object1.position = Vector3(-0.5, 0, 1);
 
     object1.indices = {
         0, 1, 2, 2, 3, 0,
         4, 5, 6, 6, 7, 4,
-        0, 1, 6, 6, 7, 0,
         1, 2, 5, 5, 6, 1,
+        0, 1, 6, 6, 7, 0,
         2, 3, 4, 4, 5, 2,
         3, 0, 7, 7, 4, 3};
+
+    object1.scale = Vector3(0.5, 1.6, 1);
+
+    object1.position = Vector3(-0.5, 0, 1);
 }
 
 void SetVariables(Application& app)
